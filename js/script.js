8868cfb97590d854d8747f559a0ec50905f00564
abdfurqon <kokofurqon@gmail.com>
2024-10-10 22:38:24 +0700

    config css dan js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 // Ini javascript
+
+console.log("Hello bro");
+
+function validateForm() {
+    console.log("validasi terpanggil");
+    const inputSuhu = document.getElementById("input-suhu").value;
+    
+    
+    if (inputSuhu == '') {
+        alert("Inputan Anda kosong!")
+    } else {
+        calculate(inputSuhu);
+    }
+}
+
+function calculate(value) {
+    let result = parseInt(value) * 2;
+    document.getElementById("hasil-suhu").value = result;
+    document.getElementById("cara-hitung-suhu").value = value + ' x 2 = ' + result;
+
+}
+
+function reset() {
+    document.getElementById("input-suhu").value = '';
+    document.getElementById("hasil-suhu").value = '';
+    document.getElementById("cara-hitung-suhu").value = '';
+}
